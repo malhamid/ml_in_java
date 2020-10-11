3ef039045f1916884c1630544b6ae85c87e12550
Mohammed Alhamid <mohammedalhamid@mohammeslabbook.lan>
2020-10-11 11:42:31 -0400

    uodating weka example

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,5 +1,6 @@
 package com.tutorial;
 
+import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -14,5 +15,26 @@ public class Main {
         System.out.println("The number of loaded instances is: " + dataInstances.numInstances());
 
         System.out.println("data:" + dataInstances.toString());
+
+        // Identifying the label index
+        dataInstances.setClassIndex(dataInstances.numAttributes() - 1);
+
+        System.out.println("The number of attributes in the dataset: " + dataInstances.numAttributes());
+        System.out.println("The number of classes: " + dataInstances.numClasses());
+
+        // printing the column name
+        for(int i=0; i < dataInstances.numAttributes(); i++){
+            System.out.println("Column " + i + "->" + dataInstances.attribute(i));
+        }
+
+        // Creating a decision free classifier
+        J48 treeClassifier = new J48();
+
+        treeClassifier.setOptions(new String[] { "-U" });
+
+        treeClassifier.buildClassifier(dataInstances);
+
+        System.out.println(treeClassifier);
+
     }
 }
